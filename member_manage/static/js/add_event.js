a5c7577a4ec7b88d04dc95f159abc5d1f8bc49d3
deosanta <santanurag_deo@mail.com>
2025-06-29 16:05:23 +0530

    Add Event functionality is added- js modified for aadidtional fields

--- member_manage/static/js/add_event.js
+++ member_manage/static/js/add_event.js
@@ -4,7 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
         const name = document.getElementById('event_name').value.trim();
         const date = document.getElementById('event_date').value;
         const instructor = document.getElementById('instructor_id').value;
-        if (!name || !date || !instructor) {
+        const location = document.getElementById('event_location').value.trim();
+        const state = document.getElementById('event_state').value.trim();
+        const country = document.getElementById('event_country').value.trim();
+        const district = document.getElementById('event_district').value.trim();
+
+        if (!name || !date || !instructor || !location || !state || !country || !district) {
             alert('Please fill all required fields.');
             e.preventDefault();
         }
